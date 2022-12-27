7013593fde3ad727c321afa4bf3833128c4a36e9
nikogoli <k_m_naoto@yahoo.co.jp>
2022-12-28 00:15:11 +0900

    余計なファイルを削除

--- code.js
+++
@@ -1,8 +0,0 @@
-// @deno-types="https://esm.sh/monaco-editor@0.34.1/monaco.d.ts"
-import loader from 'https://esm.sh/@monaco-editor/loader@1.3.2'
-
-
-loader.init().then(mona => {
-    console.log({mona})
-    console.log({editor: mona.editor})
-})
